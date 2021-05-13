221f40658339e16f47ba0610451cd5f4b1659ed2
GeorGius11 <79843910+GeorGius11@users.noreply.github.com>
2021-05-13 14:46:26 +0300

    Home Work 5

---
+++ HomeWork5/Task4/Infrastructure/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Infrastructure
+{
+    public struct student
+    {
+        string name;
+        string surname;
+        int[] marks;
+
+        public student(string name, string surname, int m1, int m2, int m3)
+        {
+            this.name = name;
+            this.surname = surname;
+            marks = new int[] { m1, m2, m3 };
+        }
+
+        public double avg
+        {
+            get { return (marks[0] + marks[1] + marks[2]) / 3f; }
+        }
+
+        public override string ToString() => $"{name} {surname} {marks[0]} {marks[1]} {marks[2]}";
+
+    }
+}
---
+++ HomeWork5/Task4/Presenter/Calc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task4.Infrastructure;
+using Task4.Models;
+using Task4.Views;
+
+namespace Task4.Presenter
+{
+    class Calc
+    {
+        Model model;
+        View view;
+        public Calc(View view)
+        {
+            model = new Model();
+            this.view = view;
+        }
+
+        public void FillStudents()
+        {
+            view.Print("Enter the path to the file:\n--> ");
+            string path = view.Input(); 
+            if (File.Exists(path))
+            {
+                List<student> students = model.FillStudents(@path);
+                view.PrintLine("\nContent of the file:\n");
+                foreach (var item in students)
+                    view.PrintLine(item.ToString());
+
+                view.PrintLine("\n=========================\n");
+                view.PrintLine("The worst students:\n");
+                List<student> Worst = model.ThreeWorst(students);
+                foreach (var item in Worst)
+                    view.PrintLine(item.ToString());
+                view.PrintLine("");
+            }
+            else
+                view.PrintLine("File does not exist\n");
+        }
+    }
+}
