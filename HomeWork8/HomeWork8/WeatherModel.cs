7e2765c3533d348db23691e4da8eed3f81ea7672
GeorGius11 <79843910+GeorGius11@users.noreply.github.com>
2021-05-24 20:24:05 +0300

    HomeWork8

---
+++ HomeWork8/HomeWork8/WeatherModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8
+{
+    class WeatherModel
+    {
+        int t, w; string h;
+        public string TEMPERATURE { get { return $"Temperature: {t} ℃"; } set { int.TryParse(value, out t); } }
+        public string WIND { get { return $"Wind speed: {w} m/s"; } set { int.TryParse(value, out w); } }
+        
+        public string DATE { get { return $"{DAY}.{MONTH}.{YEAR}"; } set { DATE = value; } }
+
+        public string DAY { get; set; }
+        public string MONTH { get; set; }
+        public string YEAR { get; set; }
+        public string HOUR { get { return $"Time: {h}:00"; } set { h = value; } }
+        public string City { get; internal set; }
+    }
+}
